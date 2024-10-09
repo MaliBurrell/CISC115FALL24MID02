f83f25ce133aff85c2b74958c5f712d6d19a155f
Ian-McDowell-NCC <ian.mcdowell@student.northampton.edu>
2024-10-08 23:55:49 -0400

    Update Assignment1_McDowell.cpp

--- Assignment1/Assignment1_McDowell.cpp
+++ Assignment1/Assignment1_McDowell.cpp
@@ -5,7 +5,8 @@ int main() {
     //Declaring variables
     float age = 16.5;
     float gradAge = 18.5; 
-    int timeUntilGrad = gradAge - age; //calculating how many years until I graduate.
+    float timeUntilGrad = gradAge - age; //calculating how many years until I graduate.
+    int timeUntilGradRound = (int)timeUntilGrad;
     // Doing feet and inches seperately and combining them
     float heightFeet = 5;
     float heightInches = 11;
@@ -17,6 +18,7 @@ int main() {
     //Printing the variables
     cout << "My name is " << fullName << endl;
     cout << "I have taken a class in C++ and HTML before but I have forgotten a lot" << endl;
+    cout << "I once got hit in the head with a bowling ball" << endl;
     cout << "I am " << age << " years old" << endl;
     cout << "I am " << heightFeet << "'"<< heightInches << endl; 
     cout << "That is " << heightTotal << " feet" << endl;
