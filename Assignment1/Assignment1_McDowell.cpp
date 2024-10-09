43c44484045309123abae6bd934dd128dbf844da
Ian-McDowell-NCC <ian.mcdowell@student.northampton.edu>
2024-10-08 23:46:52 -0400

    Update Assignment1_McDowell.cpp

--- Assignment1/Assignment1_McDowell.cpp
+++ Assignment1/Assignment1_McDowell.cpp
@@ -15,12 +15,12 @@ int main() {
     float heightTotal = heightFeet + heightInches/12;
 
     //Printing the variables
-    cout << "My name is " << full_name << endl;
+    cout << "My name is " << fullName << endl;
     cout << "I have taken a class in C++ and HTML before but I have forgotten a lot" << endl;
     cout << "I am " << age << " years old" << endl;
     cout << "I am " << heightFeet << "'"<< heightInches << endl; 
     cout << "That is " << heightTotal << " feet" << endl;
-    cout << "I want to graduate with my degree when I am " << grad_age << " years old" << endl;
+    cout << "I want to graduate with my degree when I am " << gradAge << " years old" << endl;
     cout << "That is "<< timeUntilGrad << " years from now" << endl;
     cout << "I want an " << grade << " in this class" << endl;
     return 0;
